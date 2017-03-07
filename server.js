559a60a6008e4124e453c85a54d5e137b787900e
STEPHEN NORTHOVER <snorthov@ca.ibm.com>
2017-03-07 15:41:04 -0600

    clean up code

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
 /*eslint-env node:true, node*/
 
-var GET_ASTROS = "/astros";
-var ASTROS_JSON = "public/data/astros.json";
-
 var fs = require('fs');
 var http = require('http');
 var express = require('express');
 var app = express();
 
+var GET_ASTROS = "/astros";
+var ASTROS_JSON = "public/astros.json";
+var GET_ASTROS_OPEN_NOTIFY = "http://api.open-notify.org/astros.json";
+
 // Serve static content from /public
 app.use('/', express.static(__dirname + "/public"));
 
 app.get(GET_ASTROS, /* @callback */ function(req, resp) {
-	http.get("http://api.open-notify.org/astros.json", function(resp2) {
+	http.get(GET_ASTROS_OPEN_NOTIFY, function(resp2) {
 		var body = "";
-		resp2.on("data", function(d) {
-			body += d;
+		resp2.on("data", function(data) {
+			body += data;
 		});
 		resp2.on("end", function() {
 			resp.send(JSON.parse(body));
@@ -24,23 +25,23 @@ app.get(GET_ASTROS, /* @callback */ function(req, resp) {
 });
 
 // GET the people in space (unused)
-app.get(GET_ASTROS + "1", function (req, res) {
+app.get(GET_ASTROS + "1", /* @callback */ function (req, res) {
 	res.sendfile(ASTROS_JSON);
 });
 
 // GET the people in space (unused)
-app.get(GET_ASTROS + "2", function (req, res) {
+app.get(GET_ASTROS + "2", /* @callback */ function (req, res) {
 	fs.readFile(ASTROS_JSON, "utf8", function (err, data) {
 		if (err) {
 			//TODO - send better error message
 			res.send(err);
 		} else {
-			res.send(data);
+			res.send(JSON.parse(data));
 		}
 	});
 });
 
-// listen for requests on the host at a port
-var host = process.env.VCAP_APP_HOST || 'localhost';
-var port = process.env.VCAP_APP_PORT || 3000;
-app.listen(port, host);
+var port = process.env.PORT || process.env.VCAP_APP_PORT || 3000;
+app.listen(port, function() {
+	console.log('Server running on port: %d', port);
+});
